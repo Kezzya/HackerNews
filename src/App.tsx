93d6c8931779bd4e456dc3bdbca1f8840640097e
Nikita <kobzevnikita0@mail.ru>
2022-10-26 15:13:04 +0300

    setup global scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import './App.css';
-import React from 'react'
-
-function App() : JSX.Element {
-  return (
-    <div></div>
-  );
+function App(): JSX.Element {
+  return <div>123</div>;
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 module.exports = {
-    entry: './index.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+  entry: "./index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: { presets: ["es2015", "react"] },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  loaders: [
+    {
+      test: /\.(woff|woff2|eot|ttf|svg)$/,
+      loader: "file-loader",
+      options: { name: "[name].[ext]", outputPath: "fonts/" },
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { test: /\.js$/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] }}
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
+  ],
+  devtool: "source-map",
 };
