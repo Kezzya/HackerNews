938d7d74bc8d0636dba75d560629affc24e07b24
Nikita <kobzevnikita0@mail.ru>
2022-10-26 21:38:17 +0300

    added packages for semantic ui css

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -2,8 +2,12 @@ import { NewsList } from "../../components/newsList/newsList";
 import styles from "./mainPage.module.scss";
 function MainPage(): JSX.Element {
   return (
-    <div className={styles.mainContent}>
-      <NewsList />
+    <div className={styles.wrapMainPage}>
+      <div className={styles.mainContent}>
+        <NewsList />
+      </div>
+
+      <img src="refresh-page.svg" className={styles.refreshImg} />
     </div>
   );
 }
